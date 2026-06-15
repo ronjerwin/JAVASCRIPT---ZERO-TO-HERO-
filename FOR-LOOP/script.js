0142e01db3e2d61e7a64ab14821fd2076bfc7c1f
Ron Jerwin Registrado <ronjerwin.dvcode@gmail.com>
2026-06-15 10:59:16 +0800

    For Loop

---
+++ FOR-LOOP/script.js
@@ -0,0 +1,12 @@
+// WHAT IS A FOR LOOP?
+// >> A CONTROL FLOW STATEMENT THAT REPEATEDLY EXECUTES A BLOCK OF CODE AS LONG AS THE SPECIFIED CONDITION IS TRUE.
+// >> IT IS HIGHLY EFFICIENT FOR ITERATING OVER A KNOWN NUMBER OF TIMES OR THROUGH DATA COLLECTIONS.
+
+// >> SYNTAX:
+//          for(initialization; condition; incrementation){ do some code }
+
+// EXAMPLE:
+
+  for(let i = 1; i <= 10; i++) {
+    console.log(`Count: ${i}`);
+  }
