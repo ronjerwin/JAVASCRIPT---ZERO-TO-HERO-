1b62b2c8172e13a13234d2852925e3619f838a44
ronjerwin <rronjerwin@gmail.com>
2026-06-29 10:09:14 +0800

    Arrays Review

--- ARRAYS/script.js
+++ ARRAYS/script.js
@@ -4,7 +4,7 @@
 
 // EXAMPLE:
 // ARRAYS CAN STORE MIXED DATA TYPES (STRINGS, NUMBERS, BOOLEANS, OBJECTS).
-const fruits = ['BANANA', 5, TRUE, { NAME: 'RJ' }];
+const fruits = ['BANANA', 5, { NAME: 'RJ' }];
 
 // ESSENTIAL ARRAY METHODS:
 fruits.push('APPLE');      // ADDS AN ELEMENT TO THE END.
@@ -13,3 +13,52 @@ fruits.unshift('ONION');   // ADDS AN ELEMENT TO THE BEGINNING (SHIFTING OTHERS 
 fruits.shift();            // REMOVES THE FIRST ELEMENT (SHIFTING OTHERS TO THE LEFT).
 fruits.sort();             // SORTS ELEMENTS ALPHABETICALLY (NOTE: BE CAREFUL WITH NUMBERS!).
 fruits.reverse();          // REVERSES THE ORDER OF ELEMENTS.
+
+
+
+
+
+
+const alphabets = ['f', 'v', 'q', 'd', 'e', 'a', 'k', 'h', 'i', 'j', 'g', 'l', 'm', 'z', 'o', 'p', 'c', 'r', 's', 't', 'u', 'b', 'w', 'x', 'y', 'n'];
+
+alphabets.sort();
+console.log(alphabets)
+
+const numbers = [15, 8, 6, 4, 5, 3, 7, 2, 9, 14, 11, 12, 13, 10, 1];
+numbers.sort((a, b) => a - b);
+console.log(numbers);
+const fruits1 = ['banana', 'apple', 'orange', 'lemon', 'watermelon', 'cucumber', 'coconut', 'pomegranate'];
+fruits1.sort((a, b) => a.localeCompare(b));
+console.log(fruits1);
+
+
+function renderProducts(data) {
+ data.products.map((product) => {
+      const newElement = document.createElement('div');
+      newElement.classList.add('newElementContainer')
+      newElement.innerHTML = `<p>${product.name}</p>
+                             <p>${product.price}</p>
+                             <p>${product.categories.join(', ')}</p>`
+
+        document.body.appendChild(newElement)
+    })
+
+}
+
+async function AllProducts() {
+  const response = await fetch('products.json');
+
+  try {
+    if(!response.ok) {
+      throw new Error('File not found!')
+    }
+
+    const data = await response.json();
+   renderProducts(data);
+  }
+  catch(error) {
+    console.error(error);
+  }
+}
+
+AllProducts();
