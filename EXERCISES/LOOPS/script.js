64356193733f18d966d592750ee75bc9f6175539
ronjerwin <rronjerwin@gmail.com>
2026-06-29 10:22:31 +0800

    Loops

---
+++ EXERCISES/LOOPS/script.js
@@ -0,0 +1,31 @@
+
+
+
+
+// FOR LOOP
+
+  // for(let i = 1; i <= 100; i++) {
+  //   console.log(i + ": " + 'Sorry po!');
+  // }
+
+
+// WHILE LOOP
+
+  // const text = 'Pasensya na';
+  // let count = 0;
+
+  // while(count < 100) {
+  //   count++;
+  //   console.log(count + ": " + text);
+  // }
+
+
+// DO WHILE
+
+  // const text = 'Pasensya na';
+  // let count = 0;
+
+  // do {
+  //   count++;
+  //   console.log(count + ": " + text);
+  // }while(count < 100);
